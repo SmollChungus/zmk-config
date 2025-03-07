e1dc3235ef08fb5040994d514846bfa5a021114c
smollchungusm <mpamuller@msn.com>
2025-03-07 13:57:12 +0100

    refactor to stm32f411

--- boards/arm/icebreaker/board.cmake
+++ boards/arm/icebreaker/board.cmake
@@ -1,7 +1,6 @@
 # Copyright (c) 2021 The ZMK Contributors
 # SPDX-License-Identifier: MIT
 
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+# STM32F411 configuration
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
