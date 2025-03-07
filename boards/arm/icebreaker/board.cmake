bc3269e16afd7496b2c741cdcac7ca27678665a3
smollchungusm <mpamuller@msn.com>
2025-03-07 14:25:52 +0100

    refactor bootloader n partitions

--- boards/arm/icebreaker/board.cmake
+++ boards/arm/icebreaker/board.cmake
@@ -4,3 +4,4 @@
 # STM32F411 configuration
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
