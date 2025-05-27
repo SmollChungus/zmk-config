19b68ae8912da4f97b2dd39b6c609feba4151c01
smollchungusm <mpamuller@msn.com>
2025-05-27 14:48:20 +0200

    ayay linit

---
+++ boards/arm/cleaver/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2021 The ZMK Contributors
+# SPDX-License-Identifier: MIT
+
+# STM32F411 configuration
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
